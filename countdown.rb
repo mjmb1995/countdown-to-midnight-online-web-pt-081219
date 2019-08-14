faf4b24f358a437b31792ba5f33c24fac50ef18e
Mark Bihag <markbihag330@gmail.com>
2019-08-14 05:24:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(n)
   end
     return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  while n > 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
